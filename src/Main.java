868e0d5dc5b07d211fbdae679b77e40f04a6686b
Alexander Sandberg <Alexandersandbergnya@gmail.com>
2023-11-20 11:53:23 +0100

    del 1 av g del är klar

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! gittest");
+Scanner scanner = new Scanner(System.in);
+System.out.println("skiv in text rad för rad. skiv stop för att avsluta.");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("stop")) {
+                break;
+            }
+            System.out.println("Du skrev men för att avsluta skiv Stop: " + input);
+        }
+
+        scanner.close();
+
     }
 
 
