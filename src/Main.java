199687cac337048c0fd2e57d856452b07560e18d
Alexander Sandberg <Alexandersandbergnya@gmail.com>
2023-11-20 12:30:13 +0100

    inte klar

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,18 @@
 import java.util.Scanner;
+// dett är klasen som ta emot input andra klassen är till för att räkna raderna
 public class Main {
     public static void main(String[] args) {
 Scanner scanner = new Scanner(System.in);
-int antalRader = 0;
-int antalTecken =0;
 System.out.println("skiv in text rad för rad. skiv stop för att avsluta.");
         while (true) {
             String input = scanner.nextLine();
             if (input.equalsIgnoreCase("stop")) {
                 break;
             }
-            antalRader++;
-            antalTecken += input.length();
-            System.out.println("Du skrev men för att avsluta skiv Stop: " + input);
-            System.out.println("Antal rader (exklusive 'stop'): " + antalRader);
-            System.out.println("Antal tecken (exklusive 'stop'): " + antalTecken);
         }
-// test git
-
+        line_counter_class ob= new line_counter_class();
+        System.out.println(line_counter_class.x);
         scanner.close();
-// teest för git
     }
 
 
---
+++ src/line_counter_class.java
@@ -0,0 +1,4 @@
+public class line_counter_class {
+    static int x = 5;
+
+}
