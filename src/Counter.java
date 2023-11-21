00670b722e171e8690824a6c3a0fc18bbd448023
Alexander Sandberg <Alexandersandbergnya@gmail.com>
2023-11-21 18:04:45 +0100

    finnas fel kommer att fixats snart nästa dag

--- src/Counter.java
+++ src/Counter.java
@@ -1,3 +1,9 @@
 public class Counter {
+    int antalRader = 0;
+    int antalTecken = 0;
 
+    antalRader++;
+    antalTecken += input.length();
+    System.out.println("Antal rader (exklusive 'stop'): " + antalRader);
+        System.out.println("Antal tecken (exklusive 'stop'): " + antalTecken);
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 // skapa en klass som tar emot user data
         Counter myObj2 = new Counter();
         User input = new User();
-
+// skapa så att när man skiver stop stängs progravmet av med loop och break
         String userInput = input.getUserInput();
         // Skriv ut användarens input
         System.out.println("Du skrev: " + input);
