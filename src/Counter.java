82254b95d818f4a304f74c7a0db4cb1653b9d058
Alexander Sandberg <Alexandersandbergnya@gmail.com>
2023-11-21 18:18:46 +0100

    problem att få stop att fungera igen

--- src/Counter.java
+++ src/Counter.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Counter {
-    int antalRader = 0;
-    int antalTecken = 0;
-//
-    antalRader++;
-    antalTecken += input.length();
-    System.out.println("Antal rader (exklusive 'stop'): " + antalRader);
+
+
+    public String getCounterInput() {
+        int antalRader = 0;
+        int antalTecken = 0;
+        antalRader++;
+        antalTecken += input.length();
+        System.out.println("Antal rader (exklusive 'stop'): " + antalRader);
         System.out.println("Antal tecken (exklusive 'stop'): " + antalTecken);
+
+        return input;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,15 @@
 public class Main {
     public static void main(String[] args) {
 
-// skapa en klass som tar emot user data
+
         Counter myObj2 = new Counter();
         User input = new User();
 // skapa så att när man skiver stop stängs progravmet av med loop och break
+     //   while (true) {
+
+       //     if (input.equalsIgnoreCase("stop")) {
+       //         break;
+      //      }
         String userInput = input.getUserInput();
         // Skriv ut användarens input
         System.out.println("Du skrev: " + input);
