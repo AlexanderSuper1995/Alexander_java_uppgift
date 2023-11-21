caef19be94c01d3d4a1a919a2acffec8009d7203
Alexander Sandberg <Alexandersandbergnya@gmail.com>
2023-11-21 18:06:53 +0100

    finnas fel kommer att fixats snart nästa dagx

--- src/Counter.java
+++ src/Counter.java
@@ -1,7 +1,7 @@
 public class Counter {
     int antalRader = 0;
     int antalTecken = 0;
-
+//
     antalRader++;
     antalTecken += input.length();
     System.out.println("Antal rader (exklusive 'stop'): " + antalRader);
