bc9870625e7dd60b7a6eaaa3cac2ea3b81758424
Alexander Sandberg <Alexandersandbergnya@gmail.com>
2023-11-21 12:20:07 +0100

    test git

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
 // skapa en klass som tar emot user data
-        // Skapa en klas som skiver ut från  USER klass
         Counter myObj2 = new Counter();
         User input = new User();
 
-        // Använd UserInputHandler för att få användarens input
         String userInput = input.getUserInput();
-
         // Skriv ut användarens input
         System.out.println("Du skrev: " + input);
     }
