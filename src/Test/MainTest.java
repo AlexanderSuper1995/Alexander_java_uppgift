ca611c7ee89c64121e527fb093c1936e08c045b4
Alexander Sandberg <Alexandersandbergnya@gmail.com>
2023-11-29 10:37:24 +0100

    måste jobba med testerna

--- src/Test/MainTest.java
+++ src/Test/MainTest.java
@@ -1,58 +1,52 @@
 package Test;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
+
+    int actual = 4;
+    int expetad = 0;
     int characterCount = 0;
     int  lineCount = 0;
     String args = "stop";
     // TEST FÖR STOP
     @Test
-    void test1() {
+    void test_testar_stop_och_Count_line() {
         System.out.println("Skriv in text rad för rad. Skriv 'stop' för att avsluta.");
         String input;
         while (true) {
             input = "stop";
+            expetad = input.length();
+
             System.out.println(input);
             if (input.equalsIgnoreCase("stop")) {
+                assertEquals(actual, expetad);
+
                 break;
+
             }
         }
-        int actual = 4;
-        int expetad = 0;
-        expetad = input.length();
-        assertEquals(actual, expetad);
 
     }
 
     @Test
     void test2() {
-        lineCount++;
-
-        System.out.println("Antal rader: " + lineCount);
-
-
-    }
-
-    @Test
-    void test3() {
-        characterCount = characterCount + args.length();
-        System.out.println("Antal rader: " + args.length());
-
-
+        int expetad = 0;
+        System.out.println("Antal rader: " + expetad);
+       int actual = 0;
+        expetad++;
+        actual++;
+        assertEquals(actual, expetad);
 
     }
+
+
     @Test
     void test4() {
         var statistics2 = new MainTest();
-
         statistics2.updateStatistics();
         characterCount = characterCount + args.length();
-
         lineCount++;
-
         int actual = 1;
         assertEquals(actual,lineCount);    }
     @Test
